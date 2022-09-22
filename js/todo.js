494eeb8cfc03c4e38bbe60bd1804e69cc3b2fd84
Echchi <112493260+Echchi@users.noreply.github.com>
2022-09-23 07:20:27 +0900

    Created greetins/ clock/ quotes/ todo

---
+++ js/todo.js
@@ -0,0 +1,38 @@
+const toDoForm = document.querySelector(".todo-form");
+const toDoInput = document.querySelector(".todo-form input");
+const toDoList = document.querySelector(".todo-list");
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("toDos", JASON.Stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "💥";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+
+function handleTodoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newToDo);
+    paintToDo(newToDo);
+    saveToDos(); 
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit);
